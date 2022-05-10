aa3c97a36d5ce098ce5b23a1794ab6346e98efd9
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-05-10 19:55:29 +0600

    complete the route setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appoinment from './Pages/Appoinment/Appoinment';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Home from './Pages/Home/Home';
+import Rivews from './Pages/Rivews/Rivews';
+import Login from './Pages/Login/Login'
+import Navbar from './Pages/Shared/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/appoinment' element={<Appoinment></Appoinment>}></Route>
+        <Route path='/rivews' element={<Rivews></Rivews>}></Route>
+        <Route path='/contactUs' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Appoinment/Appoinment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appoinment = () => {
+    return (
+        <div>
+            <h1>this is appoinment page</h1>
+        </div>
+    );
+};
+
+export default Appoinment;
