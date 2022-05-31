a5993c623a7671ce9365d405090cf590958440a7
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-05-31 11:04:09 +0600

    updated booking modal

--- src/Pages/Appoinment/BokingModal.js
+++ src/Pages/Appoinment/BokingModal.js
@@ -24,7 +24,7 @@ const BokingModal = ({ date, treatment, setTreatment, refetch }) => {
             phone: event.target.phone.value,
         }
 
-        fetch('https://cryptic-mesa-43832.herokuapp.com', {
+        fetch('https://cryptic-mesa-43832.herokuapp.com/booking', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
