192c5f70575518606d2d275f9a22a8cb397ba7e2
Jonied-mirza-shakib <MDSHAKIB01088@gmail.com>
2022-05-31 11:24:27 +0600

    updated checkOut form page

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -73,7 +73,7 @@ const CheckoutForm = ({ appointment }) => {
                 appointment: _id,
                 transectionId: paymentIntent.id
             }
-            fetch(`https://cryptic-mesa-43832.herokuapp.com/${_id}`, {
+            fetch(`https://cryptic-mesa-43832.herokuapp.com/booking/${_id}`, {
                 method: "PATCH",
                 headers: {
                     "authorization": `Bearer ${localStorage.getItem('accessToken')}`,
